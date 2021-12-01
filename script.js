38b0d84bf8c696436e2945f6eb6500a15f79038f
ibrahim <ibrahimea@code.edu.az>
2021-12-01 15:39:48 +0400

    Create

---
+++ script.js
@@ -0,0 +1,51 @@
+let company={
+    name: "Liverpool",
+    createYear= "1937"
+}
+
+let team={
+    name: "Galatasaray",
+    createYear: 1905
+}
+
+let eat={
+    name: "Pringles",
+    createYear:1998
+}
+
+let arr=[company,team,Eat] ;
+let checkAll=arr.forEach(x=> x.name=="Pringles");
+console.log(checkAll);
+
+
+
+
+
+// class Student{
+//     constructor(name,lastname,age){
+//         this.name=name;
+//         this.lastname=lastname;
+//         this.age=age
+ 
+//     }
+//      st1 = new Student("Samir","Aliyev",19);
+//      st2 = new Student("Famil","Aliyev",25);
+//      st3 = new Student("Qabil","Aliyev",58);
+//      st4 = new Student("Zakir","Aliyev",44);
+//  }
+ 
+//  class Group extends Person{
+ 
+//      constructor(id,groupname,studentCount,name,lastname,age){
+//          super(name,lastname,age)
+//          this.id=id;
+//          this.group=group;
+//          this.studentCount=studentCount;
+//      }
+    
+
+//  }
+//  const group1 = new Group('5A');
+//  group1.add(st1);
+
+
